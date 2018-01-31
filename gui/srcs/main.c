ab3bf9ac607b5861473c71071e0f5abec140f59e
jon-finkel <nfinkel@student.42.fr>
2018-01-31 18:03:40 +0100

    Stuff.

--- gui/srcs/main.c
+++ gui/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/31 00:10:40 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/01/31 16:10:27 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/01/31 16:27:29 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ int			main(void)
 {
 	void		*mlx;
 
-	KEEPATITBRA(mlx = mlx_new_window(mlx_init(), 500, 500, "Filler"));
+	KEEPATITBRA(mlx = mlx_new_window(mlx_init(), 1920, 1080, "Filler"));
 	KTHXBYE;
 }
