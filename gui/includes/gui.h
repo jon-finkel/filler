129fa057d4a00656a82e73550b5fd522dc5f82e3
jon-finkel <nfinkel@student.42.fr>
2018-02-01 14:16:31 +0100

    Stuff.

--- gui/includes/gui.h
+++ gui/includes/gui.h
@@ -6,7 +6,7 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/31 00:11:03 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/01/31 16:10:28 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/02/01 13:47:34 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,20 @@
 # define GUI_H
 
 # include "../../libft/includes/libft.h"
-# include "../minilibx_macos/mlx.h"
+# include "../../includes/mlx/mlx.h"
+
+# define WIN_X 1200
+# define WIN_Y 600
+
+typedef struct		s_mlx
+{
+	char			p1[512];
+	char			p2[512];
+	int				x;
+	int				y;
+	void			*id;
+	void			*map;
+	void			*win_ptr;
+}					t_mlx;
 
 #endif
--- gui/srcs/main.c
+++ gui/srcs/main.c
@@ -6,16 +6,32 @@
 /*   By: nfinkel <nfinkel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/31 00:10:40 by nfinkel           #+#    #+#             */
-/*   Updated: 2018/01/31 16:27:29 by nfinkel          ###   ########.fr       */
+/*   Updated: 2018/02/01 13:49:26 by nfinkel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/gui.h"
 
+#define MLX (mlx->id)
+#define PATH "../ressources/map.xpm"
+#define TITLE_WIN "Filler gui"
+
+static t_mlx			*init_mlx(t_mlx *mlx)
+{
+	FAILZ(mlx = (t_mlx *)ft_memalloc(sizeof(t_mlx)), NULL);
+	KEEPATITBRA(mlx->id = mlx_init());
+	mlx->x = WIN_X;
+	mlx->y = WIN_Y;
+	KEEPATITBRA(mlx->win_ptr = mlx_new_window(MLX, WIN_X, WIN_Y, TITLE_WIN));
+	GIMME(mlx);
+}
+
 int			main(void)
 {
-	void		*mlx;
+	t_mlx		*mlx;
 
-	KEEPATITBRA(mlx = mlx_new_window(mlx_init(), 1920, 1080, "Filler"));
+	if (!(mlx = init_mlx(NULL)))
+		ft_fatal("General error");
+	mlx_loop(MLX);
 	KTHXBYE;
 }
